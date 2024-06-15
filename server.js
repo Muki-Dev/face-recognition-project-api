8d409ba60464740d0c8358f8f4bb421289c170f3
Muki <mukidev881@gmail.com>
2024-06-16 01:21:26 +0200

    Connecting to the database

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors')
+const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 3301,
+    user: 'postgres',
+    password: '',
+    database: 'smart-brain',
+  },
+});
+
+console.log(db.select('*').from('users'));
 
 const app = express();
 

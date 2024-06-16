2f258310a00363dd27ff9a668f8025d24bfc9bff
Muki <mukidev881@gmail.com>
2024-06-16 03:04:07 +0200

    update register

--- server.js
+++ server.js
@@ -68,19 +68,36 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'))
 })
 
-app.post('/register', (req,res) => {
-	const {name,email,password} = req.body;
-	return db('users')
-		.returning('*')
-		.insert({
-		name: name,
-		email:email,
-		joined: new Date()
-	})
-	.then(user => {
-		res.json(user[0])
-	})
-	.catch(err => res.status(400).json('unable to register'));
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+            // loginEmail[0] --> this used to return the email
+            // TO
+            // loginEmail[0].email --> this now returns the email
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req,res) => {

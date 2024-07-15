741ddc53b256bcf7238e90a6cdc9b92b1e5b4797
Muki Dev <mukidev881@gmail.com>
2024-07-15 13:10:21 +0000

    add return

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
 const registerHandler = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if(!email || !name || !password){
-    res.status(400).json('incorrect form submission');
+    return res.status(400).json('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
